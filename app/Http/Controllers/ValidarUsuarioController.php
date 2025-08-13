e217f0f271dddb073a20a91b0a3073fa970c2ca0
Ygarciaa <ygarcia@estudiante.ceibal.edu.uy>
2025-08-13 15:28:36 -0300

    Agregué una función para mandarle a la api de usuarios ese token, validarlo y después recibir el id del usuario

--- app/Http/Controllers/ValidarUsuarioController.php
+++ app/Http/Controllers/ValidarUsuarioController.php
@@ -1,1 +1,19 @@
+<?php
 
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Conexion;
+
+class UsuariosApiService
+{
+    public function ObtenerUsuarioDesdeAPI_Usuarios($token) {
+        $token = request()->bearerToken();
+
+        $response = http::withToken($token)
+        ->get('https://127.0.0.1:8000/api/validate');
+
+        if(!($response->successful() && isset($response['id']))){
+            abort(401, 'Token inválido');
+        } return $response ['id'];
+    }
+}
